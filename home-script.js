cac17be235b502e87d801ee9533e0d6bc7701607
Kyle Dominic Lomigo <91366618+KyleDom@users.noreply.github.com>
2022-10-10 11:31:10 +0800

    modified script for home

--- home-script.js
+++ home-script.js
@@ -1,7 +1,11 @@
-let list = document.getElementById('list');
-let addBtn = document.getElementById('add-Btn');
 
-addBtn.addEventListener('click', function(){
-let txt = prompt('Student name');
+const postLoad = () => {
+  let list = document.getElementById('list');
+  const addBtn = document.querySelector('#button');
+addBtn.onclick = () => {
+  let txt = prompt('Student name');
   list.innerHTML += '<li class="list-group-item">' + txt + '</li>';
-});
+}
+}
+
+window.onload = postLoad
